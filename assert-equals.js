4214a912bfb5b6321c616fa4e00113072dbc8442
Eunbyul <las4134@gmail.com>
2023-07-21 13:48:50 +0100

    Update test code

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,61 @@
-function assertEquals(expect, actual) {}
+function assertEquals(expect, actual) {
+  // String check
+  // Check whether both expect and actual are strings, if true, check if expect is not equal to actual.
+  // If they are not the same, throw an error.
+  // First attempt
+  if (typeof expect === 'string' && typeof actual === 'string') {
+      if (expect !== actual) {
+          throw new Error(`Expected ${expect} but found ${actual}`);
+      }
+  }
 
-module.exports = assertEquals
+  // Refactored version for string check
+  if (typeof expect === 'string' && expect !== actual) {
+      throw new Error(`Expected ${expect} but found ${actual}`);
+  }
+
+  // Number check
+  if (typeof expect === 'number' && expect !== actual) {
+      throw new Error(`Expected ${expect} but found ${actual}`);
+  }
+
+  // Type check
+  if (typeof expect !== typeof actual) {
+      throw new Error(`Expected type ${typeof expect} but found type ${typeof actual}`);
+  }
+}
+
+// Array length check 
+// Check whether both expect and actual are arrays.
+// If true, then check expect.length and actual.length.
+// If the lengths are different, throw an error.
+if (Array.isArray(expect) && Array.isArray(actual)) {
+  if (expect.length !== actual.length) {
+      throw new Error(`Expected array length ${expect.length} but found ${actual.length}`);
+  }
+}
+
+// Array element check 
+// Check whether both are arrays.
+// If true, loop through each element.
+// If expect[i] is not equal to actual[i], throw an error.
+if (Array.isArray(expect) && Array.isArray(actual)) {
+  for (let i = 0; i < expect.length; i++) {
+      if (expect[i] !== actual[i]) {
+          throw new Error(`Expected ${expect[i]} but found ${actual[i]}`);
+      }
+  }
+}
+
+// Check whether both are arrays.
+// If true, loop through each element.
+// If the type of each expect element is not equal to the type of the actual element, throw an error.
+if (Array.isArray(expect) && Array.isArray(actual)) {
+  for (let i = 0; i < expect.length; i++) {
+      if (typeof expect[i] !== typeof actual[i]) {
+          throw new Error(`Expected ${typeof expect[i]} but found ${typeof actual[i]}`);
+      }
+  }
+}
+
+module.exports = assertEquals;
--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -1,13 +1,66 @@
 const assertEquals = require('./assert-equals')
 
-describe('assertEquals', () => {
+
   describe('when expected and actual are the same string', () => {
-    it('returns without throwing an error', () => {
+    it('throws an error', () => {
       expect(() => assertEquals('abc', 'abc')).not.toThrow()
-    })
-  })
+    });
+  });
 
   describe('when expected and actual are different strings', () => {
-    it.todo('throws an error')
-  })
-})
+    it('throws an error', () => {
+      expect(() => assertEquals('abcdef', 'abc')).toThrow('Expected "abcdef" but found "abc"');
+    });
+  });
+
+  describe('when expected and actual are the same number', () => {
+    it('returns without throwing an error ', () => {
+      expect(() => assertEquals(1, 1)).not.toThrow()
+    });
+  });
+
+  describe('when expected and actual are the different number', () => {
+    it('throws an error ', () => {
+      expect(() => assertEquals(1, 2)).toThrow('Expected "1" but found "2"')
+    });
+  });
+
+  describe('when expected and actual are the different type', () => {
+    it('throws an error ', () => {
+      expect(() => assertEquals(1, '1')).toThrow('Expected type number but found type string')
+    });
+  });
+
+  describe('when expected and actual are the same length', () => {
+    it('returns without throwing an error', () => {
+      expect(() => assertEquals(['a', 'b', 'c'], ['a', 'b', 'c'])).not.toThrow()
+    });
+  });
+
+  describe('when expected and actual are the different length', () => {
+    it('throws an error ', () => {
+      expect(() => assertEquals(['a', 'b', 'c'], ['a', 'b'])).toThrow('Expected array length 2 but found 3')
+    });
+  });
+
+  describe('when expected and actual are the different element', () => {
+    it('throws an error ', () => {
+      expect(() => assertEquals(['a', 'b'], ['a', 'd'])).toThrow('Expected "b" but found "d"')
+    });
+  });
+
+  describe('when expected and actual are the same element', () => {
+    it('throws an error ', () => {
+      expect(() => assertEquals(['a', 'b', '1'], ['a', 'b', '1'])).not.toThrow()
+    });
+  });
+
+  describe('when expected and actual are the different element type', () => {
+    it('throws an error ', () => {
+      expect(() => assertEquals(['a', 'b', 1], ['a', 'b', '1'])).toThrow('Expected 1 but found "1"')
+    });
+  });
+
+
+
+
